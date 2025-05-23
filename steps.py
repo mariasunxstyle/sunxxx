de0d8b0ad2eb211338fa90ac2f87a2cfb6714dfb
mariasunxstyle <tohmian@mail.ru>
2025-05-23 17:49:48 +0300

    Add files via upload

--- steps.py
+++ steps.py
@@ -1,71 +1,86 @@
 steps = [
-    {"step": 1, "duration_min": 8, "positions": [
-        {"name": "Лицом вверх", "min": 4},
-        {"name": "На животе", "min": 4}
+    {"step": 1, "duration_min": 8.0, "positions": [
+        {"name": "Лицом вверх", "min": 1.5},
+        {"name": "На животе", "min": 1.5},
+        {"name": "Левый бок", "min": 1.0},
+        {"name": "Правый бок", "min": 1.0},
+        {"name": "В тени", "min": 3.0}
     ]},
-    {"step": 2, "duration_min": 9, "positions": [
-        {"name": "Лицом вверх", "min": 3},
-        {"name": "На животе", "min": 3},
-        {"name": "Левый бок", "min": 3}
+    {"step": 2, "duration_min": 9.0, "positions": [
+        {"name": "Лицом вверх", "min": 2.0},
+        {"name": "На животе", "min": 2.0},
+        {"name": "Левый бок", "min": 1.0},
+        {"name": "Правый бок", "min": 1.0},
+        {"name": "В тени", "min": 3.0}
     ]},
-    {"step": 3, "duration_min": 14, "positions": [
-        {"name": "Лицом вверх", "min": 3},
-        {"name": "На животе", "min": 3},
-        {"name": "Левый бок", "min": 4},
-        {"name": "Правый бок", "min": 4}
+    {"step": 3, "duration_min": 14.0, "positions": [
+        {"name": "Лицом вверх", "min": 3.0},
+        {"name": "На животе", "min": 3.0},
+        {"name": "Левый бок", "min": 1.5},
+        {"name": "Правый бок", "min": 1.5},
+        {"name": "В тени", "min": 5.0}
     ]},
-    {"step": 4, "duration_min": 20, "positions": [
-        {"name": "Лицом вверх", "min": 4},
-        {"name": "На животе", "min": 4},
-        {"name": "Левый бок", "min": 6},
-        {"name": "Правый бок", "min": 6}
+    {"step": 4, "duration_min": 20.0, "positions": [
+        {"name": "Лицом вверх", "min": 5.0},
+        {"name": "На животе", "min": 5.0},
+        {"name": "Левый бок", "min": 2.5},
+        {"name": "Правый бок", "min": 2.5},
+        {"name": "В тени", "min": 5.0}
     ]},
-    {"step": 5, "duration_min": 30, "positions": [
-        {"name": "Лицом вверх", "min": 6},
-        {"name": "На животе", "min": 6},
-        {"name": "Левый бок", "min": 9},
-        {"name": "Правый бок", "min": 9}
+    {"step": 5, "duration_min": 27.0, "positions": [
+        {"name": "Лицом вверх", "min": 7.0},
+        {"name": "На животе", "min": 7.0},
+        {"name": "Левый бок", "min": 3.0},
+        {"name": "Правый бок", "min": 3.0},
+        {"name": "В тени", "min": 7.0}
     ]},
-    {"step": 6, "duration_min": 40, "positions": [
-        {"name": "Лицом вверх", "min": 8},
-        {"name": "На животе", "min": 8},
-        {"name": "Левый бок", "min": 12},
-        {"name": "Правый бок", "min": 12}
+    {"step": 6, "duration_min": 38.0, "positions": [
+        {"name": "Лицом вверх", "min": 9.0},
+        {"name": "На животе", "min": 9.0},
+        {"name": "Левый бок", "min": 5.0},
+        {"name": "Правый бок", "min": 5.0},
+        {"name": "В тени", "min": 10.0}
     ]},
-    {"step": 7, "duration_min": 60, "positions": [
-        {"name": "Лицом вверх", "min": 12},
-        {"name": "На животе", "min": 12},
-        {"name": "Левый бок", "min": 18},
-        {"name": "Правый бок", "min": 18}
+    {"step": 7, "duration_min": 48.0, "positions": [
+        {"name": "Лицом вверх", "min": 12.0},
+        {"name": "На животе", "min": 12.0},
+        {"name": "Левый бок", "min": 7.0},
+        {"name": "Правый бок", "min": 7.0},
+        {"name": "В тени", "min": 10.0}
     ]},
-    {"step": 8, "duration_min": 90, "positions": [
-        {"name": "Лицом вверх", "min": 18},
-        {"name": "На животе", "min": 18},
-        {"name": "Левый бок", "min": 27},
-        {"name": "Правый бок", "min": 27}
+    {"step": 8, "duration_min": 60.0, "positions": [
+        {"name": "Лицом вверх", "min": 15.0},
+        {"name": "На животе", "min": 15.0},
+        {"name": "Левый бок", "min": 10.0},
+        {"name": "Правый бок", "min": 10.0},
+        {"name": "В тени", "min": 10.0}
     ]},
-    {"step": 9, "duration_min": 120, "positions": [
-        {"name": "Лицом вверх", "min": 24},
-        {"name": "На животе", "min": 24},
-        {"name": "Левый бок", "min": 36},
-        {"name": "Правый бок", "min": 36}
+    {"step": 9, "duration_min": 85.0, "positions": [
+        {"name": "Лицом вверх", "min": 20.0},
+        {"name": "На животе", "min": 20.0},
+        {"name": "Левый бок", "min": 15.0},
+        {"name": "Правый бок", "min": 15.0},
+        {"name": "В тени", "min": 15.0}
     ]},
-    {"step": 10, "duration_min": 150, "positions": [
-        {"name": "Лицом вверх", "min": 30},
-        {"name": "На животе", "min": 30},
-        {"name": "Левый бок", "min": 45},
-        {"name": "Правый бок", "min": 45}
+    {"step": 10, "duration_min": 110.0, "positions": [
+        {"name": "Лицом вверх", "min": 25.0},
+        {"name": "На животе", "min": 25.0},
+        {"name": "Левый бок", "min": 20.0},
+        {"name": "Правый бок", "min": 20.0},
+        {"name": "В тени", "min": 20.0}
     ]},
-    {"step": 11, "duration_min": 150, "positions": [
-        {"name": "Лицом вверх", "min": 30},
-        {"name": "На животе", "min": 30},
-        {"name": "Левый бок", "min": 45},
-        {"name": "Правый бок", "min": 45}
+    {"step": 11, "duration_min": 150.0, "positions": [
+        {"name": "Лицом вверх", "min": 35.0},
+        {"name": "На животе", "min": 35.0},
+        {"name": "Левый бок", "min": 25.0},
+        {"name": "Правый бок", "min": 25.0},
+        {"name": "В тени", "min": 30.0}
     ]},
-    {"step": 12, "duration_min": 150, "positions": [
-        {"name": "Лицом вверх", "min": 30},
-        {"name": "На животе", "min": 30},
-        {"name": "Левый бок", "min": 45},
-        {"name": "Правый бок", "min": 45}
+    {"step": 12, "duration_min": 190.0, "positions": [
+        {"name": "Лицом вверх", "min": 45.0},
+        {"name": "На животе", "min": 45.0},
+        {"name": "Левый бок", "min": 30.0},
+        {"name": "Правый бок", "min": 30.0},
+        {"name": "В тени", "min": 40.0}
     ]}
 ]
